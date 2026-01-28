1c5d5780cbcd7faea8f18759648aa7f3b8bf48f5
ABC-HungHo <hung.ho@abcdigital.io>
2026-01-28 19:49:57 +0700

    update

--- src/support/hooks.ts
+++ src/support/hooks.ts
@@ -4,22 +4,22 @@ import { CustomWorld } from "./world";
 import { BasePage } from "../pages/basePage";
 
 Before(async function (this: CustomWorld) {
-  this.browser = await chromium.launch({
-  headless: false,
-});
-  this.context = await this.browser.newContext();
-  this.page = await this.context.newPage();
-  this.basePage = new BasePage(this.page)
+    this.browser = await chromium.launch({
+        headless: false,
+    });
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+    this.basePage = new BasePage(this.page);
 });
 
 After(async function (this: CustomWorld) {
-  if (this.page) {
-    await this.page.close();
-  }  
-  if (this.context) {
-    await this.context.close();
-  }
-  if (this.browser) {
-    await this.browser.close();
-  }
+    if (this.page) {
+        await this.page.close();
+    }
+    if (this.context) {
+        await this.context.close();
+    }
+    if (this.browser) {
+        await this.browser.close();
+    }
 });
--- src/support/world.ts
+++ src/support/world.ts
@@ -1,17 +1,17 @@
 import { Browser, BrowserContext, Page } from "@playwright/test";
 import { IWorldOptions, setWorldConstructor, World } from "@cucumber/cucumber";
 import { BasePage } from "../pages/basePage";
-import { config } from "../support/config"
+import { config } from "../support/config";
 
 export class CustomWorld extends World {
-  browser!: Browser;
-  context!: BrowserContext;
-  page!: Page;
-  basePage!: BasePage;
-  config = config;
-  constructor(options: IWorldOptions) {
-    super(options);
-  }
+    browser!: Browser;
+    context!: BrowserContext;
+    page!: Page;
+    basePage!: BasePage;
+    config = config;
+    constructor(options: IWorldOptions) {
+        super(options);
+    }
 }
 
 setWorldConstructor(CustomWorld);
